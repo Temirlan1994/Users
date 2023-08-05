83f3be5d1b9e8b9dc2188d1059c3ba3da5916919
Kasht <kubtimur1@mail.com>
2023-08-05 11:50:11 +0600

    Added the function of changeng users information

--- src/main/java/main/Authorization.java
+++ src/main/java/main/Authorization.java
@@ -1,10 +1,14 @@
 package main;
 
+import Entity.City;
 import Entity.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
+
+import java.lang.reflect.Type;
+import java.util.List;
 import java.util.Scanner;
 
 public class Authorization {
@@ -33,11 +37,45 @@ public class Authorization {
             System.out.println("Город: "+user.getCity().getName());
             System.out.println("Индекс города: "+user.getCity().getIndex());
 
+            System.out.println("Изменить данные [1]");
+            System.out.println("Просмотреть информацию о пользователе [2]");
+            System.out.print("Выберите действие!");
+
+            String action = sc.nextLine();
+            if(action.equals("1")){
+                System.out.print("Введите новое имя: ");
+                String firstName = sc.nextLine();
+                System.out.print("Введите новую фамилию: ");
+                String lastName = sc.nextLine();
+
+                TypedQuery<City> cityQuery = manager.createQuery(
+                        "select c from City c", City.class
+                );
+                List<City> cities = cityQuery.getResultList();
+                for (City city : cities) {
+                    System.out.println(city.getName()+" ["+city.getId()+"]");
+                }
+                System.out.print("Введите id другого город из списка: ");
+                String cityId = sc.nextLine();
+                City city = manager.find(City.class, cityId);
+
+                user.setFirst_name(firstName);
+                user.setLast_name(lastName);
+                user.setCity(city);
+
+            }else if(action.equals("2")){
+                System.out.println("Id: "+user.getId());
+                System.out.println("Имя: "+user.getFirst_name());
+                System.out.println("Фамилия: "+user.getLast_name());
+                System.out.println("Город: "+user.getCity().getName());
+                System.out.println("Индекс города: "+user.getCity().getIndex());
+            }
+
             manager.getTransaction().commit();
         }catch (Exception e){
             manager.getTransaction().rollback();
             e.printStackTrace();
         }
-
     }
+
 }
